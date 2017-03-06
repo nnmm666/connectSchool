f9780ea52626db94e8126c0986045e9568c4bc9d
Sejin Park <nnmm666@naver.com>
2017-03-06 14:52:45 +0900

    commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name connectSchoolApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the connectSchoolApp
+ */
+angular.module('connectSchoolApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name connectSchoolApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the connectSchoolApp
+ */
+angular.module('connectSchoolApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
