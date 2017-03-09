b822a3218d104a727521858fc01bc0ce09250b35
chanlee <wonchan.lee@gmail.com>
2017-03-09 11:46:28 +0900

    fix syntax error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
 var app = express();
 var basedir = path.join(path.dirname(__dirname), 'connectSchool');
 
-app.use('/bower_components', express.static(basedir + '/bower_components', {maxAge: ;'7d'})); // 10 sec.
+app.use('/bower_components', express.static(basedir + '/bower_components', {maxAge: '7d'})); // 10 sec.
 app.use('/app/styles', express.static(basedir + '/app/styles', {maxAge: 1000*10})); // 10 sec.
 app.use(express.static(basedir + '/app', {maxAge: 1000*10}));
 app.use(express.static(basedir + '/dist', {maxAge: 1000*10}));
